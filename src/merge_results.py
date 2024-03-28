112f7e6a4f32ff3d27bd464c40151036860fa5cc
PasiniSamuele <samu.pasini98@gmail.com>
2024-03-28 15:48:26 +0100

    merge pipeline

--- src/merge_results.py
+++ src/merge_results.py
@@ -3,40 +3,47 @@ import json
 import os
 import pandas as pd
 
+def unify_string_format(string):
+    return string.lower().replace(" ", "_").replace('/', '-').replace('_or_', '-').replace('_-_', '-')
+
 def merge_results(opt):
     #read categories file
     with open(opt.categories_file, 'r') as file:
         categories = json.load(file)
     #put all the keys of category, that is a dict, in lowestring
-    categories = {k.lower(): v for k, v in categories.items()}
+    #categories = {k.lower(): v for k, v in categories.items()}
+    #put all the keys substituting spaces by underscores
+    categories = {unify_string_format(k): v for k, v in categories.items()}
     #get all the json files in the source folder
     files = [f for f in os.listdir(opt.source_folder) if f.endswith('.json')]
     #create new dataframe
-    data = pd.DataFrame(columns=["year", "data","debate", "candidate", "party", "incumbent", "party_incumbent", "winner", "home_state", "persuasion_technique", "persuasion_technique_category", "starting_index","ending_index", "score"])
+    data = pd.DataFrame(columns=["year", "date","debate", "candidate", "party", "incumbent", "party_incumbent", "winner", "home_state", "persuasion_technique", "persuasion_technique_category", "starting_index","ending_index", "score"])
     #read all the json files
     for file in files:
         with open(opt.source_folder + file, 'r') as file:
             json_data = json.load(file)
             for candidate in json_data["candidates"]:
                 for annotation_k, annotation_v in candidate["annotations"]["entities"].items():
-                    row = {
-                        "year": json_data["year"],
-                        "data": json_data["data"],
-                        "debate_name": json_data["debate"],
-                        "candidate": candidate["name"],
-                        "party": candidate["party"],
-                        "incumbent": candidate["incumbent"],
-                        "party_incumbent": candidate["party_incumbent"],
-                        "winner": candidate["winner"],
-                        "home_state": candidate["home_state"],
-                        "persuasion_technique": annotation_k,
-                        "persuasion_technique_category": categories[annotation_k.lower()],
-                        "annotations": candidate["annotations"],
-                        "starting_index": annotation_v["indices"][0],
-                        "ending_index": annotation_v["indices"][1],
-                        "score": annotation_v["score"]
-                    }
-                    data.loc[len(data)] = row
+                    for annotation in annotation_v:
+                        key = unify_string_format(annotation_k)
+                        row = {
+                            "year": json_data["year"],
+                            "date": json_data["date"],
+                            "debate": json_data["debate_name"],
+                            "candidate": candidate["name"],
+                            "party": candidate["party"],
+                            "incumbent": candidate["incumbent"],
+                            "party_incumbent": candidate["party_incumbent"],
+                            "winner": candidate["winner"],
+                            "home_state": candidate["home_state"],
+                            "persuasion_technique": annotation_k,
+                            "persuasion_technique_category": categories[key],
+                            "annotations": candidate["annotations"],
+                            "starting_index": annotation["indices"][0],
+                            "ending_index": annotation["indices"][1],
+                            "score": annotation["score"]
+                        }
+                        data.loc[len(data)] = row
     #save output csv
     data.to_csv(opt.dest, index=False)
 
